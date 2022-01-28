38fa59a78a34d27aaffd3a0997ff9ba70d4e960b
Alimammadzada <tu201906128@aztu.local>
2022-01-28 19:30:01 +0400

    initial

---
+++ ImtahanBack/Areas/Manage/Controllers/AccountController.cs
@@ -0,0 +1,72 @@
+﻿using ImtahanBack.Areas.ViewModel;
+using ImtahanBack.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ImtahanBack.Areas.Manage.Controllers
+{
+    [Area("manage")]
+    public class AccountController : Controller
+    {
+        private readonly RoleManager<IdentityRole> _role;
+        private readonly SignInManager<AppUser> _sign;
+        private readonly UserManager<AppUser> _user;
+
+        public AccountController(RoleManager<IdentityRole> role, SignInManager <AppUser> sign, UserManager<AppUser> user)
+        {
+            _role = role;
+            _sign = sign;
+            _user = user;
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult Login()
+        {
+            return View();
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Login(AdminLoginViewModel AdminVM)
+        {
+            AppUser admin = _user.Users.FirstOrDefault(x => x.NormalizedUserName == AdminVM.UserName.ToUpper() && x.IsAdmin);
+            if (admin == null)
+            {
+                ModelState.AddModelError("", "Parol ve ya ad sehvdir");
+                return View();
+            }
+           
+         
+           var result= await _sign.PasswordSignInAsync(admin, AdminVM.Password, false, false);
+            if (!result.Succeeded)
+            {
+                ModelState.AddModelError("", "Parol ve ya ad sehvdir");
+                return View();
+            }
+          
+                return RedirectToAction("index", "dashboard");
+        }
+        public async Task<IActionResult> LogOut()
+        {
+            await _sign.SignOutAsync();
+            return RedirectToAction("login", "account");
+        }
+
+        //public async Task<IActionResult> CreateRole()
+        //{
+        //    var role1 = await _role.CreateAsync(new IdentityRole("SuperAdmin"));
+        //    var role2 = await _role.CreateAsync(new IdentityRole("Admin"));
+        //    var role3 = await _role.CreateAsync(new IdentityRole("User"));
+        //    var admin = new AppUser { FullName = "Super Admin", UserName="SuperAdmin", IsAdmin = true };
+        //    var result = await _user.CreateAsync(admin, "SuperAdmin123");
+        //    var adminrole = await _user.AddToRoleAsync(admin, "SuperAdmin");
+        //    return Ok(adminrole);
+
+        //}
+    }
+}
---
+++ ImtahanBack/Controllers/HomeController.cs
@@ -0,0 +1,44 @@
+﻿using ImtahanBack.Models;
+using ImtahanBack.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ImtahanBack.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+        private readonly DataContext _context;
+
+        public HomeController(ILogger<HomeController> logger, DataContext context)
+        {
+            _logger = logger;
+           _context = context;
+        }
+
+        public IActionResult Index()
+        {
+            HomeViewModel HomeVM = new HomeViewModel
+            {
+                Portfolios = _context.Portfolios.Where(x=>x.IsDeleted==false).ToList()
+            };
+            return View(HomeVM);
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
